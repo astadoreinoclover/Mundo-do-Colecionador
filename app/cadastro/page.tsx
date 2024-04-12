9779e141d384abb9d8ffee6dfca73e99f32c80db
Renato <renatoicardozo2001@gmail.com>
2024-04-12 19:59:34 -0300

    cadastro e login

---
+++ app/cadastro/page.tsx
@@ -0,0 +1,114 @@
+'use client'
+import { useContext, useState } from "react"
+import { useForm } from "react-hook-form"
+import { Toaster, toast } from 'sonner'
+import { ClienteContext } from "../context/ClienteContext"
+import { useRouter } from "next/navigation"
+import Swal from "sweetalert2"
+import "./cadastro.css"
+
+interface Inputs {
+    email: string
+    senha: string
+    nome: string
+    cpf: string
+    numero: string
+}
+
+
+function Cadastro(){
+
+
+    const { register, handleSubmit } = useForm<Inputs>();
+    const { mudaLogin } = useContext(ClienteContext);
+    const router = useRouter();
+
+    const [confirmarSenha, setConfirmarSenha] = useState<string>("");
+
+    async function enviaDados(data: Inputs) {
+
+        try {
+
+            if (data.senha !== confirmarSenha) {
+                Swal.fire({
+                    title: "As senhas não coincidem",
+                    icon: "error"
+                });
+                return;
+            }
+
+            const verificarCadastroResponse = await fetch("http://localhost:3004/verificar-cadastro", {
+                method: "POST",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({ email: data.email, cpf: data.cpf })
+            });
+
+            const verificarCadastro = await verificarCadastroResponse.json();
+
+            if (verificarCadastro.cadastroExistente) {
+                Swal.fire({
+                    title: "Email ou Cpf já cadastrados",
+                    icon: "warning"
+                  });
+                return;
+            }
+            const response = await fetch("http://localhost:3004/usuario", {
+                method: "POST",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                const dados = await response.json();
+                if (Number(dados.id) > 0) {
+                    mudaLogin({ id: Number(dados.id), nome: dados.nome });
+                    router.push("/");
+                } else {
+                    toast.error("Erro! Cadastro falhou");
+                }
+            } else {
+                throw new Error("Erro ao cadastrar. Por favor, tente novamente.");
+            }
+        } catch (error) {
+                console.error("Erro ao processar o cadastro:", error);
+            toast.error("Erro ao processar o cadastro. Por favor, tente novamente mais tarde.");
+        }
+    }
+
+    return(
+        <div className="max-w-7xl mx-auto mt-6 area">
+            <form className="max-w-sm mx-auto" onSubmit={handleSubmit(enviaDados)}>
+                    <div>
+                        <label htmlFor="nome" className="block text-sm font-medium text-gray-700">Nome</label>
+                        <input type="text" id="nome" className="campo mt-1 p-2 border border-gray-300 rounded-md" required {...register("nome")} />
+                    </div>
+                    <div>
+                        <label htmlFor="numero" className="block text-sm font-medium text-gray-700">Telefone</label>
+                        <input type="number" id="numero" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" className="campo mt-1 p-2 border border-gray-300 rounded-md" placeholder="Ex: (00) 90000-0000" required {...register("numero")} />
+                    </div>
+                    <div>
+                        <label htmlFor="cpf" className="block text-sm font-medium text-gray-700">CPF</label>
+                        <input type="text" id="cpf" className="campo mt-1 p-2 border border-gray-300 rounded-md" required {...register("cpf")} />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">E-mail</label>
+                        <input type="email" id="email" className="campo mt-1 p-2 border border-gray-300 rounded-md" required {...register("email")} />
+                    </div>
+                    <div>
+                        <label htmlFor="senha" className="block text-sm font-medium text-gray-700">Senha</label>
+                        <input type="password"  id="senha" className="campo mt-1 p-2 border border-gray-300 rounded-md" required {...register("senha")} />
+                    </div>
+                    <div>
+                        <label htmlFor="repeat_password" className="block text-sm font-medium text-gray-700">Confirme sua senha</label>
+                        <input type="password" name="repeat_password" id="repeat_password" className="campo mt-1 p-2 border border-gray-300 rounded-md" onChange={(e) => setConfirmarSenha(e.target.value)} required />
+                    </div>
+                <p className="text-center mt-5">
+                    <button type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Cadastrar-se</button>
+                </p>
+
+
+            </form>
+        </div>
+    )
+}
+export default Cadastro
---
+++ app/components/Cabecario.tsx
@@ -0,0 +1,76 @@
+'use client'
+import Link from "next/link"
+import { useContext } from "react"
+import { RiUserShared2Line } from "react-icons/ri"
+import { BiLogOutCircle } from "react-icons/bi"
+import { ClienteContext } from "../context/ClienteContext"
+import Swal from "sweetalert2"
+import { FaUserCircle } from "react-icons/fa"
+
+function Cabecario() {
+
+    const { idClienteLogado, nomeClienteLogado, mudaLogin } = useContext(ClienteContext)
+
+    function logout() {
+        Swal.fire({
+        title: "Confirma saída do sistema?",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sim",
+        cancelButtonText: "Não",
+        }).then((result: { isConfirmed: any }) => {
+        if (result.isConfirmed) {
+            mudaLogin({ id: null, nome: ""})
+        }
+        });
+    }
+
+    return (
+        <nav className="border-orange-200 bg-orange-500 dark:bg-orange-800 dark:border-orange-700">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="./logo.png" className="h-12" alt=" Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
+            </Link>
+            <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+                <li>
+                    <button id="dropdownDelayButton" data-dropdown-toggle="dropdownDelay" data-dropdown-delay="500" data-dropdown-trigger="hover" className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-4xl px-5 py-2.5 text-center inline-flex items-center" type="button">
+                        <FaUserCircle />
+                    </button>
+
+                    <div id="dropdownDelay" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDelayButton">
+                            <li>
+                            {idClienteLogado ?
+                                    <div className="text-center">
+                                    {nomeClienteLogado}
+                                    </div>
+                                    :
+                                     <Link href="./login" className="text-center block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Entrar</Link>
+                                }
+                            </li>
+                                <li>
+                                {idClienteLogado ?
+                                    <div className="text-center">
+                                    <span className="text-red-600 " onClick={logout} 
+                                            style={{cursor: 'pointer'}}> sair</span>  
+                                    </div>
+                                    :
+                                    <Link href="./cadastro" className="text-center block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Cadastro</Link>
+                                }
+                                </li>
+                        </ul>
+                    </div>
+
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+
+    )
+}
+export default Cabecario
